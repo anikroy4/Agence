0fe3bb5bbf69df465341d29a253eb8ffbe957332
anikroy4 <anikroy.uiu@gmail.com>
2023-08-06 21:36:25 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$('.banner_main_slider').slick({
+    arrows: false,
+    dots: true
+});
+$('.main_slide').slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:'<a href="#"><i class="fa-solid fa-angle-left"></i></a>',
+    nextArrow:'<a href="#"><i class="fa-solid fa-angle-right"></i></a>',
+    centerMode: true,
+    centerPadding: '0'
+});

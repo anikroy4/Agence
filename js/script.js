daaae6c9e924816c434f4d8ac5202e17a56d7ed2
anikroy4 <anikroy.uiu@gmail.com>
2023-09-02 10:16:07 +0600

    responsive slider addation

--- js/script.js
+++ js/script.js
@@ -12,9 +12,32 @@ $('.main_slide').slick({
     prevArrow:'<a href="#"><i class="fa-solid fa-angle-left"></i></a>',
     nextArrow:'<a href="#"><i class="fa-solid fa-angle-right"></i></a>',
     centerMode: true,
-    centerPadding: '0'
-     
+    centerPadding: '0',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 575.99,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
 
-
+    ]
 
 });
